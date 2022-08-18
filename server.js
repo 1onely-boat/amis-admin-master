1dad7db2e4d80610393e57cac41e8d592a046d18
fhz <289946784@qq.com>
2022-08-18 20:53:17 +0800

    开发前端权限管理之前

--- server.js
+++ server.js
@@ -4,16 +4,35 @@ const path = require('path');
 const reload = require('reload');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const Cookie = require('cookie-parser');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(Cookie())
 app.use(logger('dev'));
 app.use(bodyParser.json()); // Parses json, multi-part (file), url-encoded
 
+// function AuthMiddleware(req, res, next) {
+//     if (req.url.startsWith('/login')) {
+//         next()
+//         return
+//     }
+//     // 如果cookie 中不存在amisToken重定向至 login 页面
+//     if (!req.cookies) {
+//         console.log(req.cookies)
+//         res.redirect("/login")
+//         return
+//     }
+//     next();
+// }
+// app.use(AuthMiddleware)
 app.use('/public', express.static('public'));
 app.use('/pages', express.static('pages'));
 
+app.get('/login', function (req,res){
+    res.sendFile(path.join(__dirname, 'login.html'))
+})
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -38,3 +57,4 @@ reload(app)
       err
     );
   });
+
